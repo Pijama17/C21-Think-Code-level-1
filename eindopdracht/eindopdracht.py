05ee51047adb6740720788e5acfed92ab2391e64
Pijama17 <pijamafaze@gmail.com>
2021-12-03 13:18:34 +0100

    idk

--- eindopdracht/eindopdracht.py
+++ eindopdracht/eindopdracht.py
@@ -1,5 +1,38 @@
-num1 = input("enter a number: ")
-num2 = input("enter another number: ")
-result = float(num1) + float(num2)
+#ik ben van plan om een quiz te maken.
+#------------------------------------------------------------------- vraag de naam van gebruiker.
+print("hallo, ho heet jij")
+name = input ("type je naam ")
+#------------------------------------------------------------------- vraag gebruiker of ze een quiz willen maken.
+print("Welkom "+ name )
+start = input("wil je een quiz maken " + name + " ") 
 
-print(result)
+if start == "ja":
+    print("mooi zo !. ")
+
+    #------------------------------------------------------------------- eerste vraag en antwoord.
+    eerste_vraag = input("vind je de opleiding leuk? ")
+
+    if eerste_vraag == "ja":
+
+        print("dat is mooi om te horen!")
+    else:
+        print("dat is jammer. helaas kan je het nie veranderen.")
+    #------------------------------------------------------------------- tweede vraag en antwoord.
+    tweede_vraag = input("heb je al je werk af? ")
+
+    if tweede_vraag == "ja":
+        print("goedzo!")
+    else:
+        print(" ga je werk maken!")
+    #------------------------------------------------------------------- derde vraag en antwoord.
+    laaste_vraag = input("ben je trots op je zelf? ")
+
+    if laaste_vraag == "ja":
+        print("ik ben ook trots op jouw")
+    else:
+        print("ik ben ook teleurgesteld in je")
+    #------------------------------------------------------------------- zeg gedach tegen de gebruiker.
+    print("quiz einde")
+
+else:
+    print("oke fijne dag")
